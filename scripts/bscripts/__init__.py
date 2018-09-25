d978bd023d9d43305528bdc7b8e010e0b110b3a8
77695 <77695@163.com>
2018-09-25 20:30:06 +0800

    fit for py3

--- scripts/bscripts/__init__.py
+++ scripts/bscripts/__init__.py
@@ -4,4 +4,4 @@ for fn in os.listdir(os.path.dirname(__file__)):
     if fn == '__init__.py' or os.path.splitext(fn)[1] != '.py':
         continue
     trk_name = fn.replace('.py', '')
-    importlib.import_module(trk_name, trk_name)
+    importlib.import_module(trk_name, 'scripts.bscripts.{}'.format(trk_name))
